0da8f7511ad1a31bc281a77c5ff6a951f5be17bb
Martin Liversage <martin@liversage.com>
2020-10-18 16:25:39 +0200

    Revert to sending email SMTP

--- Server/Email/EmailOptions.cs
+++ Server/Email/EmailOptions.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using MailKit.Security;
 using NodaTime;
 
 namespace Frederikskaj2.Reservations.Server.Email
 {
     public class EmailOptions
     {
-        public string? SendGridApiKey { get; set; }
+        public string? SmtpHostName { get; set; }
+        public int SmtpPort { get; set; }
+        public SecureSocketOptions SocketOptions { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
         public EmailRecipient? From { get; set; }
         public EmailRecipient? ReplyTo { get; set; }
         public Uri? BaseUrl { get; set; }
