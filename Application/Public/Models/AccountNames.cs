7c76e4a2c66218ab2354e530d61c8ad407bc41b9
Martin Liversage <martin@liversage.com>
2022-10-23 14:38:31 +0200

    Update account names

--- Application/Public/Models/AccountNames.cs
+++ Application/Public/Models/AccountNames.cs
@@ -9,9 +9,9 @@ public static class AccountNames
     {
         new(PostingAccount.Income, "1201 - Udlejning af fælleslokale"),
         new(PostingAccount.Bank, "4021 - Bank"),
-        new(PostingAccount.AccountsReceivable, "Tilgodehavende beboere"),
-        new(PostingAccount.Deposits, "7601 - Depositum"),
-        new(PostingAccount.AccountsPayable, "Gæld til beboere")
+        new(PostingAccount.AccountsReceivable, "4401 – Tilgodehavende fælleslokaler"),
+        new(PostingAccount.Deposits, "7601- Depositum (IND)"),
+        new(PostingAccount.AccountsPayable, "7601 Depositum (UD)")
     };
 
     public static IEnumerable<AccountName> GetAll() => accountNames;
