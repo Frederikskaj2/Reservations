430f65df98656a0fa79eb66bd01bb2c7db0038bb
Martin Liversage <martin@liversage.com>
2022-11-11 04:25:45 +0100

    Add address to debtors in pay in drop-down

--- Client/Editors/DebtorEditor.razor.cs
+++ Client/Editors/DebtorEditor.razor.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Blazorise;
 using Frederikskaj2.Reservations.Shared.Core;
 using Microsoft.AspNetCore.Components;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Frederikskaj2.Reservations.Client.Editors;
@@ -14,8 +15,11 @@ public partial class DebtorEditor
     const string noPaymentId = "-";
 
     IReadOnlyDictionary<string, Debtor>? debtorDictionary;
+    IReadOnlyDictionary<ApartmentId, Apartment> apartments = new Dictionary<ApartmentId, Apartment>();
     Select<string> select = null!;
     string selectedPaymentId = noPaymentId;
+
+    [Inject] public ClientDataProvider DataProvider { get; set; } = null!;
 
     [Parameter] public IEnumerable<Debtor>? Debtors { get; set; }
 
@@ -34,10 +38,25 @@ public partial class DebtorEditor
         await select.ElementRef.FocusAsync();
     }
 
+    protected override async Task OnInitializedAsync()
+    {
+        var apartments = await DataProvider.GetApartmentsAsync();
+        if (apartments is not null)
+            this.apartments = apartments.ToDictionary(apartment => apartment.ApartmentId);
+    }
+
     protected override void OnParametersSet()
     {
         debtorDictionary = Debtors?.ToDictionary(debtor => debtor.PaymentId.ToString());
         selectedPaymentId = Value is not null ? Value.PaymentId.ToString() : noPaymentId;
+    }
+
+    string GetDebtorDescription(Debtor debtor)
+    {
+        var apartment = debtor.UserInformation.ApartmentId.HasValue && apartments.TryGetValue(debtor.UserInformation.ApartmentId.Value, out var a)
+            ? a
+            : null;
+        return apartment is not null ? $"({debtor.UserInformation.FullName} {apartment})" : $"({debtor.UserInformation.FullName})";
     }
 
     Task SetPaymentIdAsync(string paymentId)
