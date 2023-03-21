0754c76a1968576644048ac641b0451f71462955
Martin Liversage <martin@liversage.com>
2023-03-21 20:30:25 +0100

    Fix account names

--- Application/Public/Models/AccountNames.cs
+++ Application/Public/Models/AccountNames.cs
@@ -7,11 +7,11 @@ public static class AccountNames
 {
     static readonly IEnumerable<AccountName> accountNames = new AccountName[]
     {
-        new(PostingAccount.Income, "1201 - Udlejning af fælleslokale"),
-        new(PostingAccount.Bank, "4021 - Bank"),
-        new(PostingAccount.AccountsReceivable, "4401 – Tilgodehavende fælleslokaler"),
-        new(PostingAccount.Deposits, "7601- Depositum (IND)"),
-        new(PostingAccount.AccountsPayable, "7601 Depositum (UD)")
+        new(PostingAccount.Income, "1201 \u2013 Udlejning af fælleslokale"),
+        new(PostingAccount.Bank, "4021 \u2013 Bank"),
+        new(PostingAccount.AccountsReceivable, "4401 \u2013 Tilgodehavende fælleslokaler"),
+        new(PostingAccount.Deposits, "7601 \u2013 Depositum (IND)"),
+        new(PostingAccount.AccountsPayable, "7601 \u2013 Depositum (UD)")
     };
 
     public static IEnumerable<AccountName> GetAll() => accountNames;
